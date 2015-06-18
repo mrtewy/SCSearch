60c887f4cd1990aa405a317b2af9fe845f91823e
Tawanwong <tawanwong@softcontrol.net>
2015-06-18 19:52:03 +0700

    all

---
+++ SC Search.py
@@ -0,0 +1,34 @@
+import sublime
+import sublime_plugin
+
+import subprocess
+import webbrowser
+
+def SearchFor(text):
+    url = 'https://www.google.co.th/search?q=' + text.replace(' ','%20')
+    webbrowser.open_new_tab(url)
+
+class SoftcontrolSearchSelectionCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        for selection in self.view.sel():
+            # if the user didn't select anything, search the currently highlighted word
+            if selection.empty():
+                text = self.view.word(selection)
+
+            text = self.view.substr(selection)
+			
+            SearchFor(text)
+
+class SoftcontrolSearchFromInputCommand(sublime_plugin.WindowCommand):
+    def run(self):
+        # Get the search item
+        self.window.show_input_panel('Search Stack Overflow for', '',
+            self.on_done, self.on_change, self.on_cancel)
+    def on_done(self, input):
+        SearchFor(input)
+
+    def on_change(self, input):
+        pass
+
+    def on_cancel(self):
+        pass
